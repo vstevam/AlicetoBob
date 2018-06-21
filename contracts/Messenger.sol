8c40b2db34ee55fc326e4844b3b0811737cfa3aa
Vinicius Stevam <vstevan@gmail.com>
2018-06-21 15:33:14 +0100

    refactore claim function

--- contracts/Messenger.sol
+++ contracts/Messenger.sol
@@ -1,5 +1,6 @@
 pragma solidity ^0.4.24;
-
+/// @title A contract for sent msg and money
+/// @author vstevam
 contract Messenger{
   address public owner;
   function Messenger() public{
@@ -15,23 +16,38 @@ contract Messenger{
   //map hashing the hash
   mapping(bytes32 => Msg) public msgs;
 
-
+  /**
+   * Setting msg and amount of money to the blockchain
+   * @param  _sender    [who is sending the msg and amount of money.]
+   * @param  _receiver  []
+   * @param  _amount    [qtd of money to send.]
+   * @param  _ecMsg     [Msg encypted.]
+   * @return msg.ecMsg  [return the msg.]
+   */
   function sendMsgandMoney(address _sender, address _receiver, uint _amount, bytes32 _ecMsg) public returns(bytes32){
-    var msg = msgs[_ecMsg];
-    //setting the msg and the amount
-    msg.sender = _sender;
-    msg.receiver = _receiver;
-    msg.amount = _amount;
-    msg.ecMsg = _ecMsg;
 
-    return msg.ecMsg;
+    //setting the msg and the amount
+    msgs[_ecMsg].sender = _sender;
+    msgs[_ecMsg].receiver = _receiver;
+    msgs[_ecMsg].amount = _amount;
+    msgs[_ecMsg].ecMsg = _ecMsg;
+
+    return msgs[_ecMsg].ecMsg;
   }
 
-  function showMsgandClaimMoney(bytes32 _ecMsg, address _receiver, uint _amount) public returns(address){
-    var msg = msgs[_ecMsg];
-    require(msg.ecMsg == _ecMsg);
-      //msg.receiver.transfer(_amount);
-      return msg.receiver;
+  /**
+   * Check the hash of _ecMsg to claim the amount of money.
+   * @param   _ecMsg       msg encrypted.
+   * @param   _receiver    who is claiming the money.
+   * @param   _amount      qtd of money to claimed.
+   * @return  msg.receiver the receiver address.
+   */
+  function showMsgandClaimMoney(bytes32 _ecMsg, address _receiver, uint _amount) public payable returns(address){
+    Msg memory msgB = msgs[_ecMsg];
+    require(msgB.ecMsg == _ecMsg);
+    //msg.sender.transfer(_amount);
+    msgB.amount -= _amount;
+    return msgB.receiver;
   }
 
 }
